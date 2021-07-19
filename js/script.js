8b57c82ed5a48f403596903c0b364d9a619f405e
marcoche88 <marco.che@hotmail.it>
2021-07-19 15:21:35 +0200

    script ES1

--- js/script.js
+++ js/script.js
@@ -8,3 +8,29 @@ Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
 Dare la possibilità all’utente, attraverso 3 prompt(),
 di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 */
+
+// ESERCIZIO 1
+/*
+1- creare oggetto studente
+2- stampare tutte le proprietà dell'oggetto studente
+*/
+
+// creazione oggetto studente con proprietà: nome, cognome e età
+var student = {
+    firstName: "Mario",
+    lastName: "Rossi",
+    age: 30,
+}
+
+console.table(student);
+
+// stampa singole proprietà dell'oggetto con un ciclo for-in
+var objectDisplay = document.getElementById("object-student");
+var studentProperty = "Dati studente: ";
+
+for (var key in student) {
+    studentProperty += student[key] + " ";
+}
+
+objectDisplay.innerHTML = studentProperty;
+
