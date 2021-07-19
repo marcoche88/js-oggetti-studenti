1b23a38b5052250ab669fe4f5f5bdb0484b6a7c1
marcoche88 <marco.che@hotmail.it>
2021-07-19 15:07:35 +0200

    boilerplate + ccs + js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/*
+ES 1
+Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
+Stampare a schermo (in pagina) attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+ES 2
+Creare un array di oggetti di studenti.
+Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+Dare la possibilità all’utente, attraverso 3 prompt(),
+di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+*/
